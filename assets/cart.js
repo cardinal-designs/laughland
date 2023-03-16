621edfbc78f706944e80e887fef2d548e3680535
jackievalori <63308026+jackievalori@users.noreply.github.com>
2023-03-16 17:12:12 -0400

    cart dynamic subtotal fix

--- assets/cart.js
+++ assets/cart.js
@@ -43,21 +43,20 @@ class CartItems extends HTMLElement {
         selector: '.shopify-section'
       },
       {
-        id: 'cart-live-region-text',
-        section: 'cart-live-region-text',
-        selector: '.shopify-section'
-      },
-      {
         id: 'main-cart-footer',
         section: document.getElementById('main-cart-footer').dataset.id,
         selector: '.js-contents',
+      },
+      {
+        id: 'cart-live-region-text',
+        section: 'cart-live-region-text',
+        selector: '.shopify-section'
       }
     ];
   }
 
   updateQuantity(line, quantity, name) {
     this.enableLoading(line);
-
     const body = JSON.stringify({
       line,
       quantity,
@@ -77,10 +76,11 @@ class CartItems extends HTMLElement {
         if (cartFooter) cartFooter.classList.toggle('is-empty', parsedState.item_count === 0);
 
         this.getSectionsToRender().forEach((section => {
+          console.log(section)
           const elementToReplace =
-            document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
+            document.getElementById(section.id)?.querySelector(section.selector) || document.getElementById(section.id);
 
-          elementToReplace.innerHTML =
+          if (elementToReplace) elementToReplace.innerHTML =
             this.getSectionInnerHTML(parsedState.sections[section.section], section.selector);
         }));
 
